ff8ecc24a3592bd20d47934f0b3cd46593cce545
CaptainMesomorph <captain.mesomorph@gmail.com>
2019-06-22 15:53:25 +0800

    Added paging and sorting parameters to the GET action of ObjectController.

---
+++ Code/Tardigrade.Framework/Tardigrade.Framework/Extensions/IQueryableExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace Tardigrade.Framework.Extensions
+{
+    /// <summary>
+    /// This static class contains extension methods for IQueryable.
+    /// </summary>
+    public static class IQueryableExtension
+    {
+        /// <summary>
+        /// Apply a sort condition based upon a string of the format: <![CDATA[[<property>:ASC|DESC,]+]]>. For example,
+        /// "lastname:desc,firstname:asc". The order specified (ASC or DESC) is case insensitive.
+        /// <a href="https://www.itorian.com/2015/12/sorting-in-webapi-generic-way-to-apply.html">Sorting in WebAPI - a generic way to apply sorting</a>
+        /// </summary>
+        /// <typeparam name="T">Type associated with the IQueryable.</typeparam>
+        /// <param name="source">IQueryable this extension is applied to.</param>
+        /// <param name="sortBy">String containing the sort condition.</param>
+        /// <returns>An IQueryable with the sort condition applied.</returns>
+        /// <exception cref="ArgumentException">Formay of sortBy is invalid.</exception>
+        /// <exception cref="ArgumentNullException">source is null, or sortBy is null or empty.</exception>
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string sortBy)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+
+            if (string.IsNullOrWhiteSpace(sortBy)) throw new ArgumentNullException(nameof(sortBy));
+
+            string[] sortItems = sortBy.Trim().Split(',');
+            string sortExpression = string.Empty;
+
+            foreach (string sortItem in sortItems)
+            {
+                string[] condition = sortItem.Trim().Split(':');
+
+                if (condition.Count() != 2)
+                {
+                    throw new ArgumentException("Format of sort condition invalid; propery/order pair incorrectly defined.", nameof(sortBy));
+                }
+
+                string property = condition[0].Trim();
+                string order = condition[1].Trim();
+
+                if (string.IsNullOrWhiteSpace(property))
+                {
+                    throw new ArgumentException("Format of sort condition invalid; no property specified.", nameof(sortBy));
+                }
+
+                if (!"asc".Equals(order, StringComparison.InvariantCultureIgnoreCase) &&
+                    !"desc".Equals(order, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    throw new ArgumentException("Format of sort condition invalid; order must be ASC or DESC.", nameof(sortBy));
+                }
+
+                sortExpression += $"{condition[0].Trim()} {condition[1].Trim()},";
+            }
+
+            return source.OrderBy(sortExpression.Remove(sortExpression.Length - 1));
+        }
+    }
+}
