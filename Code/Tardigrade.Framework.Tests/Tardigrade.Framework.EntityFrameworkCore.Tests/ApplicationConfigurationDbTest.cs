6d110cf9e0e7902171bc3348406f6ec29ae9117f
CaptainMesomorph <captain.mesomorph@gmail.com>
2020-12-06 16:44:53 +0800

    Enhanced ConfigurationExtension to support more type casting options.

--- Code/Tardigrade.Framework.Tests/Tardigrade.Framework.EntityFrameworkCore.Tests/ApplicationConfigurationDbTest.cs
+++ Code/Tardigrade.Framework.Tests/Tardigrade.Framework.EntityFrameworkCore.Tests/ApplicationConfigurationDbTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System;
 using Tardigrade.Framework.Configurations;
 using Tardigrade.Framework.EntityFrameworkCore.Configurations;
 using Tardigrade.Framework.Extensions;
@@ -17,10 +18,38 @@ namespace Tardigrade.Framework.EntityFrameworkCore.Tests
         }
 
         [Theory]
+        [InlineData("Test.NullValue")]
+        public void GetBooleanSetting_NullValue_FormatException(string settingName)
+        {
+            Assert.Null(Config.GetAsBoolean(settingName));
+        }
+
+        [Theory]
+        [InlineData("Test.NullValue")]
+        public void GetEnumSetting_NullValue_ArgumentException(string settingName)
+        {
+            Assert.Null(Config.GetAsEnum<DayOfWeek>(settingName));
+        }
+
+        [Theory]
+        [InlineData("Test.NullValue")]
+        public void GetIntSetting_NullValue_FormatException(string settingName)
+        {
+            Assert.Null(Config.GetAsInt(settingName));
+        }
+
+        [Theory]
         [InlineData("database", "Test.Source")]
         public void GetSourceSetting_ValidSetting_Success(string expectedValue, string settingName)
         {
             Assert.Equal(expectedValue, Config.GetAsString(settingName));
+        }
+
+        [Theory]
+        [InlineData("Test.NullValue")]
+        public void GetStringSetting_NullValue_Success(string settingName)
+        {
+            Assert.Null(Config.GetAsString(settingName));
         }
     }
 }
